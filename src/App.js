872578209bca634df35d04d903cb4b9ad32b2b37
Uong Tri Phat <uongtriphat.wall@gmail.com>
2022-03-23 09:46:40 +0700

    BT_ReactJS_BC17_first

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import Databinding from './DataBinding/Databinding';
+import HandleEvent from './HandleEvent/HandleEvent';
+import Rendering_Conditions from './Rendering_Conditions/Rendering_Conditions';
+import Ex1StateDemo from './StateDemo/Ex1StateDemo';
+import RenderWithMap from './RenderWithMap/RenderWithMap';
+import QuanLyDanhSachPhim from './RenderWithMap/QuanLyDanhSachPhim';
+import BaiTapXemChiTietSP from './Props/BaiTapXemChiTietSP/BaiTapXemChiTietSP';
+import BaiTapGioHangRedux from './DemoRedux/BaiTapGioHangRedux/BaiTapGioHangRedux';
+import GameXucXac from './DemoRedux/GameXucXac/GameXucXac';
+import BaiTapMatKinh from './Bài tập glasses Mắt Kính/BaiTapMatKinh';
+import HomeComponent from './components/BaiTapLayout/HomeComponent';
+
+import SinhVien from './Databinding1/SinhVien';
+import Event from './Databinding1/Event';
+import DemoProps from './Props/DemoProps/DemoProps';
+import ShoesShop from './Props/ShoesShop/ShoesShop';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <BaiTapMatKinh/> */}
+      {/* <HomeComponent/> */}
+    
+      {/* <Databinding /> */}
+      {/* <SinhVien/> */}
+      {/* <Event/> */}
+      {/* <DemoProps/> */}
+      {/* <ShoesShop/> */}
+      {/* <HandleEvent /> */}
+      {/* <Rendering_Conditions /> */}
+      {/* <Ex1StateDemo /> */}
+      {/* <RenderWithMap /> */}
+      {/* <QuanLyDanhSachPhim /> */}
+      {/* <BaiTapXemChiTietSP /> */}
+      {/* <BaiTapGioHangRedux /> */}
+      <GameXucXac />
+    </div>
+  );
+}
+
+
+//   {/* <BaiTapLayOut1/> */} con bi loi import 
+export default App;
